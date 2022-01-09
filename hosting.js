541a2637cdebb5a032f12f9ac7d59b91e8f337f8
Mynul Islam <mynulakash436@gmail.com>
2022-01-09 17:08:17 +0600

    hosting

---
+++ hosting.js
@@ -0,0 +1,14 @@
+getName();//Assalamualikum vai....
+console.log(x);//undefined
+var x;
+console.log(getName);
+function getName() {
+  console.log("Assalamualikum vai....");
+}
+var getName3=function(){
+  //Behalfs like a variable
+}
+
+var getName2 =()=>{//Behalfs like a variable
+    
+}
